0abf587d3bbba505200babfd4592e495bf1fb789
xia0sheng <wangyouyu6@163.com>
2016-04-26 17:05:38 +0800

    20160426

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'ArticleController@index');
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
---
+++ resources/views/articles/lists.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Article lists page</title>
+</head>
+<body>
+	<h1>这是文章页面</h1>
+</body>
+<html>
