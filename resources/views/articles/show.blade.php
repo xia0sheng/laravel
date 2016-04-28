525b49884182e64103f44e9a161cd46522be05e1
xia0sheng <wangyouyu6@163.com>
2016-04-28 16:45:14 +0800

    edit show.blade.php

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,6 +1,15 @@
 @extends('app')
 @section('content')
-    <h1>{{$article->title}}</h1>
-    <hr>
-    <p>{{$article->content}}</p>
+    <article class="format-image group">
+        <h2 class="post-title pad">
+            <a href="/articles/{{ $article->id }}" rel="bookmark"> {{ $article->title }}</a>
+        </h2>
+        <div class="post-inner">
+            <div class="post-content pad">
+                <div class="entry custome">
+                    {{ $article->content }}
+                </div>
+            </div>
+        </div>
+    </article>
 @endsection
