63c1aa71b92a695978764ec286d26c4064485c52
xia0sheng <wangyouyu6@163.com>
2016-05-03 10:37:31 +0800

    modify title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <title>Laravel 5 教程</title>
+    <title>xia0sheng 的Laravel测试空间</title>
     <link rel='stylesheet' href="http://apps.bdimg.com/libs/bootstrap/3.3.4/css/bootstrap.min.css" type='text/css' media='all'/>
     <link rel='stylesheet' href="/css/all.css" type='text/css' media='all'/>
     <script type='text/javascript' src="/js/all.js"></script>
